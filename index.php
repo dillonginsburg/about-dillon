6afdff59d6d8334632d58ea3cbdadacba8855219
Dillon Ginsburg <dillon.ginsburg@gmail.com>
2019-04-08 16:21:52 -0600

    commit project 1 'about me'

--- index.php
+++ index.php
@@ -2,9 +2,12 @@
 <html lang="en">
 	<head>
 		<meta charset="UTF-8"
-		<title>About Dillon</title>
+		<title>About Me</title>
+		<link rel="stylesheet" href="./styles/style.css"
 	</head>
 	<body>
-		<p>Hello, my name is Dillon. I'm not great at writing about myself because I have spent a lot of time deconstructing my ego, and thus this may sound like a dating app bio.</p>
+		<h1>About Dillon</h1>
+		<img src="ME.jpg">
+		<p>Hello, my name is Dillon. I'm not great at writing about myself because I have spent a lot of time deconstructing my ego, and thus it feels as though there is not much to write. Here we go.</p>
 		<p>Hikes, bikes, cars, stars, classical music, and an analog interpersonal style engulfed in digitality. Good food and better ingredients, low key, drama free, easy chillin.</p>
 	<body/>
