ad09c7020e6c7ff23a91d8762805ec9c43fca7e6
Dillon Ginsburg <dillon.ginsburg@gmail.com>
2019-04-08 15:51:37 -0600

    commit project 1 'about me'

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8"
+		<title>About Dillon</title>
+	</head>
+	<body>
+		<p>Hello, my name is Dillon. I'm not great at writing about myself because I have spent a lot of time deconstructing my ego, and thus this may sound like a dating app bio.</p>
+		<p>Hikes, bikes, cars, stars, classical music, and an analog interpersonal style engulfed in digitality. Good food and better ingredients, low key, drama free, easy chillin.</p>
+	<body/>
