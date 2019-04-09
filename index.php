23ce309fc2c67fc697996211f47460ba9b7a49b8
eyoungcode <48844150+eyoungcode@users.noreply.github.com>
2019-04-08 18:48:51 -0600

    Update index.php

--- index.php
+++ index.php
@@ -10,6 +10,6 @@
 		<img src="ME.jpg">
 		<p>Hello, my name is Dillon. I'm not great at writing about myself because I have spent <strong>a lot</strong> of time deconstructing my ego, and thus it feels as though there is not much to write. Here we go.</p>
 		<p>Hikes, bikes, cars, stars, classical music, and an analog interpersonal style engulfed in digitality. Good food and better ingredients, low key, drama free, easy chillin.</p>
-	<img src="no drama for you mama"> 
+	<img src="no drama for you mama.jpg"> 
 	     <body/>
 
